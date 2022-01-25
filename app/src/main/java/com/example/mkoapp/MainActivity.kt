5676607de4d7014bc11562d5bdafb34ba40e2957
MrSedan <elpashevs@bk.ru>
2022-01-25 11:39:57 +0300

    UPD

--- app/src/main/java/com/example/mkoapp/MainActivity.kt
+++ app/src/main/java/com/example/mkoapp/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.example.mkoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.Request
-import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONArray
 import org.json.JSONObject
-import java.net.HttpURLConnection
-import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     private fun JSONObject.toMap(): Map<String, *> = keys().asSequence().associateWith {
@@ -28,14 +26,26 @@ class MainActivity : AppCompatActivity() {
             else            -> value
         }
     }
+    private fun setText(text: String){
+        val ed = getPreferences(MODE_PRIVATE).edit()
+        ed.putString("token",text)
+        ed.apply()
+    }
+    private fun getText(): String? {
+        val ed = getPreferences(MODE_PRIVATE)
+        return ed.getString("token", "")
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val but: Button = findViewById(R.id.button)
-        val tex: TextView = findViewById(R.id.textView)
+        val tex: TextView = findViewById(R.id.token)
         val que = Volley.newRequestQueue(this)
+        val tok = getText()
+        if (tok!=null) tex.text = tok
+        Log.d("Aboba","Created")
         but.setOnClickListener {
-            val url = "http://mskko2021.mad.hakta.pro/api/quotes"
+            /*val url = "http://mskko2021.mad.hakta.pro/api/quotes"
 
             val strReq = StringRequest(Request.Method.GET,url,
                 { response ->
@@ -47,9 +57,28 @@ class MainActivity : AppCompatActivity() {
 
                 },
                 { error -> Log.e("Aboba", error.toString()) }
-            )
-            que.add(strReq)
+            )*/
 
+            val url = "http://mskko2021.mad.hakta.pro/api/user/login"
+            val jsonBody = JSONObject()
+            jsonBody.put("email","junior@wsr.ru")
+            jsonBody.put("password","junior")
+            val strReq = JsonObjectRequest(Request.Method.POST, url, jsonBody,
+                { response ->
+                    val s = response.get("token").toString()
+                    val avatar = response.get("avatar").toString()
+                    "Your token is $s".also { tex.text = it }
+                    val intent = Intent(this, ProfileActivity::class.java)
+                    setText(s)
+                    intent.putExtra("token",s)
+                    intent.putExtra("avatar",avatar)
+                    startActivity(intent)
+                },
+                {
+                    error -> Log.e("Aboba", error.toString())
+                })
+            que.add(strReq)
         }
+
     }
 }
---
+++ app/src/main/java/com/example/mkoapp/ProfileActivity.kt
@@ -0,0 +1,32 @@
+package com.example.mkoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import java.io.InputStream
+import java.net.URL
+
+
+class ProfileActivity : AppCompatActivity() {
+    val av = findViewById<ImageView>(R.id.avatarImage)
+    private fun drawAvatar(url: String?){
+        if (url!==null){
+            Glide.with(this)
+                .load(url)
+                .into(av)
+        } else {
+            av.setImageResource(R.drawable.ic_launcher_background)
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+        val tokenStr = findViewById<TextView>(R.id.tokenStr)
+        val s = intent.getStringExtra("token")
+        val avurl = intent.getStringExtra("avatar")
+        drawAvatar(avurl)
+        "This is your token: $s".also { tokenStr.text = it }
+    }
+}
