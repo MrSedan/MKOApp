0c777fcfbe26e2927012d907375159eade6235c2
MrSedan <elpashevs@bk.ru>
2022-01-30 19:15:15 +0300

    UPD

--- app/src/main/java/com/example/mkoapp/MainActivity.kt
+++ app/src/main/java/com/example/mkoapp/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.mkoapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
+import org.json.JSONArray
 import org.json.JSONObject
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,12 +31,40 @@ class MainActivity : AppCompatActivity() {
         }
         av.clipToOutline = true
     }
+
+    private fun sortJsonArrayByKey(arr: JSONArray): JSONArray{
+        val sortedArr = JSONArray()
+        val jsonValues =  ArrayList<JSONObject>()
+        for(i in 0 until arr.length()){
+            jsonValues.add(arr.getJSONObject(i))
+        }
+        Collections.sort(jsonValues,object: Comparator<JSONObject>(){
+
+        })
+    }
     private fun showFeelings(){
-        val url = "https://mskko2021.mad.hakta.pro/api/feelings"
+        val que = Volley.newRequestQueue(this)
+        val url = "http://mskko2021.mad.hakta.pro/api/feelings"
         val sF = JsonObjectRequest(Request.Method.GET,url,null,
             {response ->
-
-            },{})
+                val feel = response.getJSONArray("data")
+                val container = findViewById<LinearLayout>(R.id.container)
+                for(i in 0 until feel.length()) {
+                    val item = feel.getJSONObject(i)
+                    val imgUrl = item.getString("image")
+                    val view = layoutInflater.inflate(R.layout.feeling_card,null,false)
+                    val newView = view.findViewById<ImageView>(R.id.feel_pic)
+                    view.findViewById<TextView>(R.id.feel_text).text = item.getString("title")
+                    //newView.setImageResource(R.drawable.ic_launcher_background)
+                    Glide.with(this@MainActivity)
+                        .load(imgUrl)
+                        .into(newView)
+                    container.addView(view)
+                }
+            },{ error ->
+                Log.e("Aboba",error.toString())
+            })
+        que.add(sF)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,5 +98,6 @@ class MainActivity : AppCompatActivity() {
         val mainTitle = findViewById<TextView>(R.id.main_title)
         val name = getSharedPreferences("mysettings", MODE_PRIVATE).getString("name","")
         "С возвращением, $name!".also { mainTitle.text = it }
+        showFeelings()
     }
 }
